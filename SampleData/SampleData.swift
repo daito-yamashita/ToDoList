8cf491c7ed01812337e8bdc4f98e983a0e52b803
ns-yamashita <daito.yamashita@na-s.jp>
2021-04-19 15:46:04 +0900

    dataSourceを作成
    なぜかエラー発生

---
+++ SampleData/SampleData.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoListCell.swift
+//  ToDoList
+//
+//  Created by daito yamashita on 2021/04/19.
+//
+
+import UIKit
+
+//struct SampleData: Hashable {
+//    
+//    let text: String
+//    let identifier = UUID()
+//    
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(identifier)
+//    }
+//    
+//    static func == (lhs: SampleData, rhs: SampleData) -> Bool {
+//        return lhs.identifier == rhs.identifier
+//    }
+//    
+//    var data = [SampleData(text: "aaa"), SampleData(text: "bbb")]
+//    
+//}
--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -21,6 +21,10 @@ class ToDoListViewController: UIViewController {
         private let identifier = UUID()
     }
     
+    let aaa = Item(title: "aaa")
+    let bbb = Item(title: "bbb")
+    let ccc = Item(title: "ccc")
+    
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     
@@ -48,6 +52,24 @@ extension ToDoListViewController {
     }
     
     func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Item>{ (cell, indexPath, item) in
+            var contentConfiguration = UIListContentConfiguration.valueCell()
+            contentConfiguration.text = item.title
+            cell.contentConfiguration = contentConfiguration
+            
+            cell.accessories = [.disclosureIndicator(), .reorder(displayed: .always)]
+            cell.accessories = [.disclosureIndicator(), .checkmark(displayed: .always)]
+        }
         
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) {
+            (collectionView, indexPath, item) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item.title)
+        }
+        
+        dataSource.reorderingHandlers.canReorderItem = { item in return true }
+        dataSource.reorderingHandlers.didReorder = { [weak self] transaction in
+            guard let self = self else { return }
+        }
     }
 }
+
