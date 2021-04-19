16c16375338e6d24c10aa8c9782fd2710ca07dcf
ns-yamashita <daito.yamashita@na-s.jp>
2021-04-19 14:24:50 +0900

    storyboardでNavigationBarを作成

---
+++ ToDoList/ToDoListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ToDoListViewController.swift
+//  ToDoList
+//
+//  Created by daito yamashita on 2021/04/19.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    enum Section {
+        case main
+    }
+    
+    struct Item: Hashable {
+        let title: String
+        init(title: String) {
+            self.title = title
+        }
+        private let identifier = UUID()
+    }
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureHierarchy()
+        configureDataSource()
+    }
+
+}
+
+extension ToDoListViewController {
+    
+    func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .red
+        view.addSubview(collectionView)
+    }
+    
+    func createLayout() -> UICollectionViewLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        return UICollectionViewCompositionalLayout.list(using: configuration)
+    }
+    
+    func configureDataSource() {
+        
+    }
+}
